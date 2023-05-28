b4839e8affd51e03a845ac773e2b7cc753f206ae
dara <dara77singh@gmail.com>
2023-05-28 16:20:49 +0530

    First Testcase written

---
+++ test/stringcalculatortdd.test.js
@@ -0,0 +1,23 @@
+// StrinCalcultor class
+class Stringcalculator {
+    constructor() {}
+}
+
+//For testing the code base in the terminal
+
+//Writing the unit testcases
+
+describe("The String Calculator using Test Driven Development", () => {
+    let calculator;
+
+    beforeEach(() => {
+        calculator = new Stringcalculator();
+    });
+
+    it("Create a simple String calculator", () => {
+        const result = calculator.Add();
+        expect(result).toBe(0);
+        const result1 = calculator.Add("");
+        expect(result1).toBe(0);
+    });
+})
