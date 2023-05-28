8dc3c96dd75a0e6c0b3d468b485d58d9e456cf3e
dara <dara77singh@gmail.com>
2023-05-28 17:16:21 +0530

    Second Testcase Passed

--- test/stringcalculatortdd.test.js
+++ test/stringcalculatortdd.test.js
@@ -1,12 +1,21 @@
 // StringCalcultor class
 class Stringcalculator {
-    constructor() {}
+  constructor() {}
 
-    Add () {//Test case passed 1
+  // Add () {//Test case passed 1
+  //     return 0;
+  // }
+
+  Add(numbers) {
+    //Test case passed 1-2
+    if (numbers !== undefined && numbers.length > 0) {
+      const numberArray = numbers.split(",").map((num) => parseInt(num));
+      const sum = numberArray.reduce((acc, num) => acc + num, 0);
+      return sum;} 
+      else {
         return 0;
     }
-
-    
+  }
 }
 
 //For testing the code base in the terminal
@@ -14,26 +23,25 @@ class Stringcalculator {
 //Writing the unit testcases
 
 describe("The String Calculator using Test Driven Development", () => {
-    let calculator;
+  let calculator;
 
-    beforeEach(() => {
-        calculator = new Stringcalculator();
-    });
+  beforeEach(() => {
+    calculator = new Stringcalculator();
+  });
 
-    it("Create a simple String calculator", () => {
-        const result = calculator.Add();
-        expect(result).toBe(0);
-        const result1 = calculator.Add("");
-        expect(result1).toBe(0);
-    });
+  it("Create a simple String calculator", () => {
+    const result = calculator.Add();
+    expect(result).toBe(0);
+    const result1 = calculator.Add("");
+    expect(result1).toBe(0);
+  });
 
-    it("Add method should return the sum of one number, two number, many number", () => {
-        const result = calculator.Add(2);
-        expect(result).toBe(2);
-        const result2 = calculator.Add("2,3");
-        expect(result2).toBe(5);
-        const result3 = calculator.Add("2, 3, 57, 78");
-        expect(result3).toBe(140); 
-
-    });
-})
+  it("Add method should return the sum of one number, two number, many number", () => {
+    const result = calculator.Add("2");
+    expect(result).toBe(2);
+    const result2 = calculator.Add("2,3");
+    expect(result2).toBe(5);
+    const result3 = calculator.Add("2, 3, 57, 78");
+    expect(result3).toBe(140);
+  });
+});
